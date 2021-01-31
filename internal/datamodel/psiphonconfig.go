b50f3a6e86e0fbc6e05cc4fea9b9cfb9bf318cee
Simone Basso <bassosimone@gmail.com>
2021-01-31 01:55:00 +0100

    fix: remove unused data structure

--- internal/datamodel/psiphonconfig.go
+++ internal/datamodel/psiphonconfig.go
@@ -3,12 +3,5 @@ package datamodel
 // PsiphonConfigRequest is the request for the PsiphonConfig API
 type PsiphonConfigRequest struct{}
 
-// PsiphonConfigRemoteServerListURL is the URL of a remote server
-type PsiphonConfigRemoteServerListURL struct {
-	URL               string
-	OnlyAfterAttempts int64
-	SkipVerify        bool
-}
-
 // PsiphonConfigResponse is the response from the PsiphonConfig API
 type PsiphonConfigResponse map[string]interface{}
