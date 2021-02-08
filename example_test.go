645d32b319a2b99be63daea0f3b9476d711ae233
Simone Basso <bassosimone@gmail.com>
2021-02-08 15:27:57 +0100

    doc: add usage examples

---
+++ example_test.go
@@ -0,0 +1,36 @@
+package apiclient_test
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/bassosimone/apiclient"
+)
+
+func ExampleURLsAPI_Call() {
+	var api apiclient.URLsAPI
+	request := &apiclient.URLsRequest{
+		CountryCode: "IT",
+		Limit:       14,
+	}
+	ctx := context.Background()
+	response, err := api.Call(ctx, request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v", response)
+}
+
+func ExampleTorTargetsAPI_Call() {
+	api := apiclient.TorTargetsAPI{
+		Token: "valid-token-here",
+	}
+	request := &apiclient.TorTargetsRequest{}
+	ctx := context.Background()
+	response, err := api.Call(ctx, request)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d", len(response))
+}
