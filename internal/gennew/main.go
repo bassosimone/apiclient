a96271b719c175f970f35a1f1b047c011e3c3501
Simone Basso <bassosimone@gmail.com>
2021-02-16 17:31:26 +0100

    fix: new.go factories should not return error
    There's no case in which they can fail, so returning an error
    provides for an error condition that we cannot test.

--- internal/gennew/main.go
+++ internal/gennew/main.go
@@ -14,8 +14,9 @@ import (
 func genapi(filep osx.File, desc *apimodel.Descriptor) {
 	typename := reflectx.Must(reflectx.NewTypeValueInfo(desc.Request)).TypeName()
 	apiname := strings.Replace(typename, "Request", "API", 1)
-	fmtx.Fprintf(filep, "// New%s creates a new instance of %s.\n", apiname, apiname)
-	fmtx.Fprintf(filep, "func New%s(clnt *Client) (*%s, error) {\n", apiname, apiname)
+	fmtx.Fprintf(filep, "// New%s creates a new instance of %s. This factory\n", apiname, apiname)
+	fmtx.Fprint(filep, "// always returns a valid, non-nil instance.\n")
+	fmtx.Fprintf(filep, "func New%s(clnt *Client) *%s {\n", apiname, apiname)
 	fmtx.Fprintf(filep, "\tapi := &%s{\n", apiname)
 	if desc.RequiresLogin {
 		fmtx.Fprintf(filep, "\t\tAuthorizer: clnt,\n")
@@ -24,7 +25,7 @@ func genapi(filep osx.File, desc *apimodel.Descriptor) {
 	fmtx.Fprintf(filep, "\t\tHTTPClient: clnt.HTTPClient,\n")
 	fmtx.Fprintf(filep, "\t\tUserAgent: clnt.UserAgent,\n")
 	fmtx.Fprintf(filep, "\t}\n")
-	fmtx.Fprintf(filep, "\treturn api, nil\n")
+	fmtx.Fprintf(filep, "\treturn api\n")
 	fmtx.Fprint(filep, "}\n\n")
 }
 
