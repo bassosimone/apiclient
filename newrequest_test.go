eed86da1f848a7924174f64df44e8cce7cff4095
Simone Basso <bassosimone@gmail.com>
2021-02-16 11:57:18 +0100

    feat: finish writing unit tests to get 100% of coverage

---
+++ newrequest_test.go
@@ -0,0 +1,58 @@
+package apiclient
+
+import (
+	"context"
+	"testing"
+)
+
+// TODO(bassosimone): baseURL should use the field
+// TODO(bassosimone): allow retry request
+// TODO(bassosimone): write test for URL path
+
+func TestMeasurementMetaNewRequestRLOkay(t *testing.T) {
+	api := &MeasurementMetaAPI{}
+	apireq := &MeasurementMetaRequest{
+		ReportID: "abc",
+		Full:     true,
+		Input:    "xyz",
+	}
+	ctx := context.Background()
+	req, err := api.newRequest(ctx, "https://ps1.ooni.io", apireq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := req.URL.Query()
+	if q.Get("report_id") != "abc" {
+		t.Fatal("invalid report_id")
+	}
+	if q.Get("full") != "true" {
+		t.Fatal("invalid full")
+	}
+	if q.Get("input") != "xyz" {
+		t.Fatal("invalid xyz")
+	}
+}
+
+func TestURLsNewRequestURLOkay(t *testing.T) {
+	api := &URLsAPI{}
+	apireq := &URLsRequest{
+		CategoryCodes: "HUMR,HACK",
+		CountryCode:   "IT",
+		Limit:         128,
+	}
+	ctx := context.Background()
+	req, err := api.newRequest(ctx, "https://ps1.ooni.io", apireq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	q := req.URL.Query()
+	if q.Get("category_codes") != "HUMR,HACK" {
+		t.Fatal("invalid category_codes")
+	}
+	if q.Get("country_code") != "IT" {
+		t.Fatal("invalid country_code")
+	}
+	if q.Get("limit") != "128" {
+		t.Fatal("invalid limit")
+	}
+}
