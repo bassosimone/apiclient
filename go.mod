3e07c4cc76802cc3f2e1365a60a9c980a0728856
Simone Basso <bassosimone@gmail.com>
2021-02-17 18:01:31 +0100

    feat: gofmt the generated files

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/hexops/gotextdiff v1.0.3
 	github.com/iancoleman/strcase v0.1.3
+	golang.org/x/sys v0.0.0-20210217105451-b926d437f341
 )
--- internal/cmd/generator/generator.go
+++ internal/cmd/generator/generator.go
@@ -1,6 +1,16 @@
 // Command generator generates code and tests.
 package main
 
+import (
+	"github.com/bassosimone/apiclient/internal/cmd/internal/fatalx"
+	"golang.org/x/sys/execabs"
+)
+
+func gofmt(filename string) {
+	cmd := execabs.Command("go", "fmt", filename)
+	fatalx.OnError(cmd.Run(), "cmd.Run failed")
+}
+
 func main() {
 	{
 		filep := mustCreateFile("generated.go")
@@ -8,9 +18,11 @@ func main() {
 		genAPIModel(filep)
 		genSwagger(filep)
 	}
+	gofmt("generated.go")
 	{
 		filep := mustCreateFile("generated_test.go")
 		defer filep.Close()
 		genTests(filep)
 	}
+	gofmt("generated_test.go")
 }
